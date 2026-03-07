91f8ff439fbd041caa9d2611eb330bd29e964073
pulidu <godagepulindu@gmail.com>
2026-03-07 09:23:11 +0530

    1st commit- Created Springboot project

---
+++ src/main/java/lk/ijse/cinebook/CineBookApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.cinebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CineBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/cinebook/CineBookApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.cinebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
