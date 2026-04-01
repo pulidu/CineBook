a79bbd21ca0620a4a5d51ac90f30c770a9916c17
pulidu <godagepulindu@gmail.com>
2026-04-01 12:07:25 +0530

    6th commit - Add AuthController to handle user authentication in CineBook

---
+++ src/main/java/lk/ijse/cinebook/controllers/AuthController.java
@@ -0,0 +1,30 @@
+package lk.ijse.cinebook.controller;
+
+import lk.ijse.cinebook.dto.response.AuthResponse;
+import lk.ijse.cinebook.dto.request.LoginRequest;
+import lk.ijse.cinebook.dto.request.RegisterRequest;
+import lk.ijse.cinebook.service.impl.AuthServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthServiceImpl authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        AuthResponse response = authService.register(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        AuthResponse response = authService.login(request);
+        return ResponseEntity.ok(response);
+    }
+}
