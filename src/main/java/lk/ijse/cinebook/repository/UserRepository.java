124b8511690b6c85a2ab392d3d05cbc9d8af02d9
pulidu <godagepulindu@gmail.com>
2026-04-01 16:45:40 +0530

    48th commit - add UserRepository for managing user data access

---
+++ src/main/java/lk/ijse/cinebook/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package lk.ijse.cinebook.repository;
+
+import lk.ijse.cinebook.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
